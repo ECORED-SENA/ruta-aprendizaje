c7b10816b8840e384b7f3fb6ebfa311dc6a420db
Francisco Lizcano <fjlizcano@misena.edu.co>
2020-07-28 17:31:04 -0500

    actualización con gulp

---
+++ gulpfile.js
@@ -0,0 +1,72 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const plumber = require('gulp-plumber');
+const prettify = require('gulp-prettify');
+const sourcemaps = require("gulp-sourcemaps");
+const autoprefixer = require('gulp-autoprefixer');
+const pug = require('gulp-pug');
+const browserSync = require('browser-sync').create();
+
+
+const src = './';
+const pub = './';
+
+// VARIABLES
+const paths = {
+    sass: {
+        src: `${src}/scss/main.scss`,
+        dest: `${pub}/css`,
+    },
+    pug: {
+        src: `${src}/pug/*.pug`,
+        dest: `${pub}`,
+    }
+};
+
+// COMPILAR PUG
+gulp.task('pug', done => {
+    gulp
+        .src(paths.pug.src)
+        // PREVIENE QUE LOS PROCESOS GULP.WATCH SE DETENGA AL ENCONTRAR UN ERROR
+        .pipe(plumber())
+        // COMPLIA PUG
+        .pipe(
+            pug({
+                locals: {}
+            })
+        )
+        // ENBELLECE EL HTML
+        .pipe(prettify({ indent_size: 4 }))
+        // GUARDA EL ARCHIVO HTML
+        .pipe(gulp.dest(paths.pug.dest))
+        // REFRESCADO DEL NAVEGADOR
+        .pipe(browserSync.stream());
+    done();
+});
+
+gulp.task("sass", function () {
+  return gulp
+    .src("./scss/main.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("./css"));
+});
+
+
+
+gulp.task('servidor', done => {
+    browserSync.init({
+        server: pub,
+        open: true,
+        notify: true
+    });
+    done();
+});
+
+
+gulp.task('watch', done => {
+    gulp.watch(paths.pug.src, gulp.series('pug'));
+    gulp.watch(paths.sass.src, gulp.series('sass'));
+    done();
+});
+
+gulp.task("default", gulp.series("pug", "sass", "servidor", "watch"));
